8616ab64e617a4b31b42d38412b5e206200c63e5
Todd Matthews <todd@heroku.com>
2010-07-01 13:59:24 -0700

    simplify redirects

--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
+  @view = "index"
   erb :index
 end
 
@@ -15,12 +16,21 @@ get '/56' do
   erb :play
 end
 
+get '/playlist' do
+  @url = "http://kalx.berkeley.edu/last24hours.php"
+  erb :play
+end
+
 __END__
 
-@@ index
+
+@@ layout
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
+    <% if @view != "index" %>
+      <meta http-equiv="refresh" content="0; url=<%= @url %>">
+    <% end %>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta http-equiv="Content-Language" content="en-us" />
     <meta name = "viewport" content = "user-scalable=no, width=device-width">
@@ -69,38 +79,22 @@ __END__
     </style>
   </head>
   <body>
-    <h1>KALX</h1>
-    <ul>
-      <li>
-        <a href="http://icecast.media.berkeley.edu:8000/kalx-128.mp3.m3u">128k</a>
-      </li>
-      <li>
-        <a href="http://icecast.media.berkeley.edu:8000/kalx-56.mp3.m3u">56k</a>
-      </li>
-      <li>
-        <a href="http://kalx.berkeley.edu/last24hours.php">Playlist</a>
-      </li>
-    </ul>
+    <%= yield %>
   </body>
   </html>
-  
+
+@@ index
+  <h1>KALX</h1>
+  <ul>
+    <li>
+      <a href="/128">128k</a>
+    </li>
+    <li>
+      <a href="/56">56k</a>
+    </li>
+    <li>
+      <a href="/playlist">Playlist</a>
+    </li>
+  </ul>
+
 @@ play
-  <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-  <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="Content-Language" content="en-us" />
-    <meta http-equiv="refresh" content="0; url=<%= @url %>">
-    <meta name = "viewport" content = "user-scalable=no, width=device-width">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <title>KALX Mobile</title>
-    <style type="text/css">
-      body {
-        background-color:#1a1a1a;
-      }
-    </style>
-  </head>
-  <body>
-  </body>
-  </html>
